bf0a400f2b98158b4f34a6ba6bb88ad7cb718796
Samuel Lando <samuel.lando@mail.concordia.ca>
2020-01-13 11:55:29 -0500

    Fix links

--- frontend/jsx/index.jsx
+++ frontend/jsx/index.jsx
@@ -1,7 +1,7 @@
 class Signature extends React.Component {
   render() {
     return(
-      <p className='signature'>
+      <p onClick={redirectToGithub} className='signature'>
         Made with {this.props.emoji} by {this.props.by}
       </p>
     );
@@ -11,9 +11,9 @@ class Signature extends React.Component {
 class Navigation extends React.Component {
   render() {
     return(
+      // TODO add planet control.
       <ul className='navigation'>
-        <li><h2>Planet Control</h2></li>
-        <li onClick={loadPageMenu}><h2>Pages</h2></li>
+        <h2 onClick={loadPageMenu}>Pages</h2>
       </ul>
     );
   }
@@ -24,7 +24,7 @@ class Home extends React.Component {
     return (
       <div id='index'>
         <Sky stars={1000} tick={20} />
-        <h1>Samuel Lando</h1>
+        <h1 onClick={redirectToGithub}>Samuel Lando</h1>
         <System planets={1} />
         <Navigation />
         <Signature emoji="❤️" by="Sam" />
@@ -38,6 +38,10 @@ function loadHome() {
     <Home />,
     document.getElementById('root')
   );
+}
+
+function redirectToGithub() {
+  window.location.href = "https://github.com/samuellando";
 }
 
 loadHome();

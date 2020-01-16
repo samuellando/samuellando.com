6074f3f52bbf7bbd6ff0bf7b9c1bc0fa1a139a7f
Samuel Lando <samuel.lando@mail.concordia.ca>
2020-01-16 15:15:30 -0500

    Add DB access methods

--- backend/model.go
+++ backend/model.go
@@ -4,7 +4,6 @@ package main
 
 import (
   "database/sql"
-  "errors"
 )
 
 type page struct {
@@ -14,21 +13,56 @@ type page struct {
 }
 
 func (p *page) getPage(db *sql.DB) error {
-  return errors.New("Not implemented")
+	return db.QueryRow("SELECT title, text FROM pages WHERE id=$1",
+		p.ID).Scan(&p.Title, &p.Text)
 }
 
 func (p *page) updatePage(db *sql.DB) error {
-  return errors.New("Not implemented")
+	_, err :=
+		db.Exec("UPDATE pages SET title=$1, text=$2 WHERE id=$3",
+			p.Title, p.Text, p.ID)
+
+	return err
 }
 
 func (p *page) deletePage(db *sql.DB) error {
-  return errors.New("Not implemented")
+	_, err := db.Exec("DELETE FROM pages WHERE id=$1", p.ID)
+
+	return err
 }
 
 func (p *page) createPage(db *sql.DB) error {
-  return errors.New("Not implemented")
+	err := db.QueryRow(
+		"INSERT INTO pages(title, text) VALUES($1, $2) RETURNING id",
+		p.Title, p.Text).Scan(&p.ID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func getPages(db *sql.DB, start, count int) ([]page, error) {
-  return nil, errors.New("Not implemented")
+	rows, err := db.Query(
+		"SELECT id, title,  text FROM products LIMIT $1 OFFSET $2",
+		count, start)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	pages := []page{}
+
+	for rows.Next() {
+		var p page
+		if err := rows.Scan(&p.ID, &p.Title, &p.Text); err != nil {
+			return nil, err
+		}
+		pages = append(pages, p)
+	}
+
+	return pages, nil
 }

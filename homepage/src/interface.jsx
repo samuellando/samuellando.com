59c1b0628fdc5f3d6ce16dcc93d6db55122ed3d2
Samuel Lando <samuel.lando@mail.concordia.ca>
2020-01-19 22:46:47 -0500

    Add interface to backend

---
+++ homepage/src/interface.jsx
@@ -0,0 +1,46 @@
+
+
+var backendURL = "http://localhost:8080";
+
+export default class Interface {
+  getPages() {
+    return ajax("GET", backendURL+"/pages")
+  }
+
+  getPage(id) {
+    return ajax("GET", backendURL+"/page/"+id)
+  }
+
+  createPage(title, text) {
+    return ajax("POST", backendURL+"/page", {title:title, text:text});
+  }
+
+  updatePage(id, title, text) {
+    return ajax("PUT", backendURL+"/page/"+id, {title:title, text:text});
+  }
+
+  deletePage(id) {
+    return ajax("DELETE", backendURL+"/page/"+id);
+  }
+}
+
+function ajax(method, url, payload) {
+  return new Promise(function(resolve, reject) {
+    var req = new XMLHttpRequest();
+    req.open(method, url);
+    req.onreadystatechange = function() {
+      if (req.readyState == 4) {
+        var res = {
+          status: req.status,
+          data: JSON.parse(req.responseText)
+        };
+        if (req.status == 200) {
+          resolve(res);
+        } else {
+          reject(res);
+        } 
+      }
+    };
+    req.send(JSON.stringify(payload));
+  });
+}

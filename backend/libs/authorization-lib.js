b2ca99fbe687d823ab8f92ef95fbb92953806dbc
Samuel Lando <samuel.lando@mail.concordia.ca>
2020-01-30 16:43:35 -0500

    Implement authorization lib

---
+++ backend/libs/authorization-lib.js
@@ -0,0 +1,91 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+
+export async function get(event) {
+    const params = {
+        TableName: process.env.authTableName,
+
+        Key: {
+        userid: event.requestContext.identity.cognitoIdentityId,
+        pageid: event.pathParameters.id,
+        }
+    };
+
+  try {
+    const result = await dynamoDbLib.call("get", params);
+    if (result.Item) {
+      // Return the retrieved level
+      return result.Item.level;
+    } else {
+      return false;
+    }
+  } catch (e) {
+    return false;
+  }
+}
+
+export async function post(event, context) {
+    if (await get(event) != 0) {
+        return false;
+    }
+
+  const data = JSON.parse(event.body);
+    const params = {
+        TableName: process.env.authTableName,
+
+        item: {
+            userid: event.userid,
+            pageid: event.pathParameters.id,
+            level: data.level,
+        }
+    };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+export async function del(event) {
+    if (await get(event) != 0) {
+        return false;
+    }
+
+    const data = JSON.parse(event.body);
+
+    const params = {
+        TableName: process.env.authTableName,
+
+        Key: {
+            userid: data.userid,
+            pageid: event.pathParameters.id,
+        }
+    };
+
+    try {
+        await dynamoDbLib.call("delete", params);
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+export async function list(event) {
+  const params = {
+    TableName: process.env.authTableName,
+
+    KeyConditionExpression: "userid = :userid",
+    ExpressionAttributeValues: {
+      ":userid": event.requestContext.identity.cognitoIdentityId,
+    }
+  };
+
+  try {
+    const result = await dynamoDbLib.call("query", params);
+    // Return the matching list of items in response body
+    return result.Items;
+  } catch (e) {
+    return false;
+  }
+}

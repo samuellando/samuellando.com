ff7c5b6b41115686e66d2fd80853d6014fbc047e
Samuel Lando <samuel.lando@mail.concordia.ca>
2020-01-31 09:14:44 -0500

    Update authorization lib

--- backend/libs/authorization-lib.js
+++ backend/libs/authorization-lib.js
@@ -1,91 +1,19 @@
-import * as dynamoDbLib from "./libs/dynamodb-lib";
+/**
+ * This library provides an API to the authorization table for the handlers.
+ */
 
-export async function get(event) {
-    const params = {
-        TableName: process.env.authTableName,
+import * as DbLib from "./database-lib";
 
-        Key: {
-        userid: event.requestContext.identity.cognitoIdentityId,
-        pageid: event.pathParameters.id,
-        }
-    };
-
-  try {
-    const result = await dynamoDbLib.call("get", params);
-    if (result.Item) {
-      // Return the retrieved level
-      return result.Item.level;
-    } else {
-      return false;
-    }
-  } catch (e) {
-    return false;
-  }
+export function addAuthorization(authorization) {
+    return DbLib.addItem(process.env.authorizationTableName, authorization);
 }
 
-export async function post(event, force) {
-    if (force && await get(event) != 0) {
-        return false;
-    }
-
-  const data = JSON.parse(event.body);
-    const params = {
-        TableName: process.env.authTableName,
-
-        item: {
-            userid: event.userid,
-            pageid: event.pathParameters.id,
-            level: data.level,
-        }
-    };
-
-  try {
-    await dynamoDbLib.call("put", params);
-    return true;
-  } catch (e) {
-    return false;
-  }
+export function removeAuthorization(userid, pageid) {
+    return DbLib.removeItem(process.env.authorizationTableName, 
+        {"userid": userid, "pageid": pageid});
 }
 
-export async function del(event) {
-    if (await get(event) != 0) {
-        return false;
-    }
-
-    const data = JSON.parse(event.body);
-
-    const params = {
-        TableName: process.env.authTableName,
-
-        Key: {
-            userid: data.userid,
-            pageid: event.pathParameters.id,
-        }
-    };
-
-    try {
-        await dynamoDbLib.call("delete", params);
-        return true;
-    } catch (e) {
-        return false;
-    }
-}
-
-export async function list(event) {
-  const params = {
-    TableName: process.env.authTableName,
-
-    KeyConditionExpression: "userid = :userid",
-    ExpressionAttributeValues: {
-      ":userid": event.requestContext.identity.cognitoIdentityId,
-    }
-  };
-
-  try {
-    const result = await dynamoDbLib.call("query", params);
-    // Return the matching list of items in response body
-    return result.Items;
-  } catch (e) {
-    return false;
-  }
+export function retrieveAuthorization(userid, pageid) {
+    return DbLib.addItem(process.env.authorizationTableName,
+        {"userid": userid, "pageid": pageid});
 }

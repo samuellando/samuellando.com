d6ef25cfa7b94e64de3b6e46a47d0e50de8e8c1b
Samuel Lando <samuel.lando@mail.concordia.ca>
2020-01-31 08:33:44 -0500

    Update response lib

--- backend/libs/response-lib.js
+++ backend/libs/response-lib.js
@@ -1,9 +1,22 @@
+/**
+ * This library provides response functions for the handlers.
+ * @param {*} body 
+ */
+
 export function success(body) {
   return buildResponse(200, body);
 }
 
 export function failure(body) {
   return buildResponse(500, body);
+}
+
+export function notFound(body) {
+  return buildResponse(404, body);
+}
+
+export function unauthorized(body) {
+  return buildResponse(401, body);
 }
 
 function buildResponse(statusCode, body) {

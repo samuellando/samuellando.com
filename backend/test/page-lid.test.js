e540503016fff62a3a7f9b48daf7ea1ccdff0fa6
Samuel Lando <samuel.lando@mail.concordia.ca>
2020-02-08 01:33:51 -0500

    Define add page test

---
+++ backend/test/page-lid.test.js
@@ -0,0 +1,99 @@
+import AWS from "aws-sdk";
+import * as pageLib from "../libs/page-lib"; 
+import * as dbLib from "../libs/database-lib"; 
+
+AWS.config.update({region:'ca-central-1'});
+var db = new AWS.DynamoDB({endpoint: 'http://localhost:8000'});
+
+const tableName = "pages";
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+
+async function createTable() {
+    var status = false;
+    db.createTable({
+            AttributeDefinitions: [{AttributeName: 'userid', AttributeType: 'S'}, {AttributeName: 'pageid', AttributeType: 'S'}],
+            TableName: tableName, 
+            KeySchema: [{AttributeName: 'userid', KeyType: 'HASH'},
+                {AttributeName: 'pageid', KeyType: 'RANGE'}],
+            ProvisionedThroughput: {
+                'ReadCapacityUnits': 5,
+                'WriteCapacityUnits': 5
+            },
+        }, (err, data) => {
+            if (err == null) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    );
+    return status;
+}
+
+async function deleteTable() {
+    var status = false;
+    await db.deleteTable({
+            TableName: tableName, 
+        },
+        (err, data) => {
+            if (err === null) {
+                status = true;
+            } else {
+                status = false;
+            }
+        }
+    ).promise();
+    return status;
+}
+
+describe('pagelib', () => {
+        beforeAll(async () => {
+                await createTable();
+                await sleep(1000);
+            }
+        );
+
+        afterAll(async () => {
+                await deleteTable();
+            }
+        );
+
+        test("add page", async () => {
+                const req = await pageLib.addPage("USER-1", "Test Title", "Test Text", true);
+                const res = await dbLib.listItems(tableName, {userid: "USER-1"});
+
+                expect(res.Count).toEqual(1);
+                expect(res.Items[0].userid).toEqual('USER-1');
+                expect(res.Items[0].title).toEqual('Test Title');
+                expect(res.Items[0].text).toEqual('Test Text');
+                expect(res.Items[0].private).toBeDefined();
+            }
+        );
+
+        test("remove page", async () => {
+            }
+        );
+
+        test("edit page", async () => {
+            }
+        );
+
+        test("retrieve page", async () => {
+            }
+        );
+
+        test("list public pages", async () => {
+            }
+        );
+
+        test("list pages", async () => {
+            }
+        );
+    }
+);

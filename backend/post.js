4492aa79c1ea7434f8bd30eff8cd71e8572cd83b
Samuel Lando <samuel.lando@mail.concordia.ca>
2020-02-15 18:07:26 -0500

    Add post handler

--- backend/post.js
+++ backend/post.js
@@ -1,30 +1,15 @@
-import uuid from "uuid";
-import * as authorizationLib from "./libs/authorization-lib";
-import * as dynamoDbLib from "./libs/dynamodb-lib";
+import * as PageLib from "./libs/page-lib";
+
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
   const data = JSON.parse(event.body);
-  const params = {
-    TableName: process.env.tableName,
+  const userid = event.requestContext.identity.cognitoIdentityId;
 
-    Item: {
-      pageid: uuid.v1(),
-      title: data.title,
-      content: data.content,
-      private: data.private,
-    }
-  };
-
-  try {
-    await dynamoDbLib.call("put", params);
-    event.pathParameters.pageid = params.Item.pageid;
-    if (authorizationLib.post(event, force=true)) {
-      return success(params.Item);
-    } else {
-      return failure({status: false});
-    }
-  } catch (e) {
-    return failure({ status: false });
+  const res = await PageLib.addPage(userid, data.title, data.text);
+  if (res) {
+    return success({status: "Page created."});
+  } else {
+    return failure({status: "Failed to create page."});
   }
 }

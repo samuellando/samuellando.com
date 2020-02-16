b19f8d9df356f3acfd92f04469626559c4fe2c10
Samuel Lando <samuel.lando@mail.concordia.ca>
2020-02-15 21:04:37 -0500

    Add delete handler

--- backend/delete.js
+++ backend/delete.js
@@ -1,22 +1,18 @@
-import * as dynamoDbLib from "./libs/dynamodb-lib";
-import * as authorizationLib from "./libs/authorization-lib";
+import * as PageLib from "./libs/page-lib";
+import * as AuthLib from "./libs/authorization-lib";
+
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
-  const params = {
-    TableName: process.env.tableName,
-    Key: {
-      pageid: event.pathParameters.id
-    }
-  };
+  const userid = event.requestContext.identity.cognitoIdentityId;
+  const pageid = event.pathParameters.id;
 
-  try {
-    if (authorizationLib.get(event) != 0) {
-      return false;
+  const auth = await AuthLib.retrieveAuthorization(userid, pageid);
+  if (auth.level <= 1) {
+    const res = await PageLib.removePage(userid, pageid);
+    if (res) {
+      return success({status: "Page deleted."});
     }
-    await dynamoDbLib.call("delete", params);
-    return success({ status: true });
-  } catch (e) {
-    return failure({ status: false });
   }
+  return failure({status: "Failed to delete page."});
 }

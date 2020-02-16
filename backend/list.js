d4f0f4791caef22299e16740a1208a327bf79975
Samuel Lando <samuel.lando@mail.concordia.ca>
2020-02-16 16:01:33 -0500

    Add listing handler

--- backend/list.js
+++ backend/list.js
@@ -1,28 +1,17 @@
-import * as dynamoDbLib from "./libs/dynamodb-lib";
-import * as authorizationLib from "./libs/authorization-lib";
+import * as PageLib from "./libs/page-lib";
+
 import { success, failure } from "./libs/response-lib";
-import * as get from './get';
 
 export async function main(event, context) {
-  const params = {
-    TableName: process.env.tableName,
+  var userid = event.requestContext.identity.cognitoIdentityId;
 
-    KeyConditionExpression: "userid = :userid",
-    ExpressionAttributeValues: {
-      ":userid": event.requestContext.identity.cognitoIdentityId
-    }
-  };
-
-  try {
-    var result = await dynamoDbLib.call("query", params);
-    const auth = authorizationLib.list(event);
-    for (var i = 0; i < auth.length; i++) {
-      event.pathParameters.pageid = auth[i].pageid;
-      results.Items = result.Items.push(await get.get(event));
-    }
-    // Return the matching list of items in response body
-    return success(result.Items);
-  } catch (e) {
-    return failure({ status: false });
+  if (userid == undefined) {
+    userid = "PUBLIC";
   }
+
+  const res = await PageLib.listPages(userid);
+  if (res) {
+    return success(res);
+  }
+  return failure({status: "Failed to list pages."});
 }

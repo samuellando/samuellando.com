edfdd5b81d0a71327c4e7143340e17c3ee96fb3e
Samuel Lando <samuel.lando@aol.com>
2020-08-28 10:59:11 -0400

    Layout resume section

--- src/components/view/Resume.vue
+++ src/components/view/Resume.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="resume">
     <Heading text="Resume" />
+    <ul>
+      <li><a href="#"><img src="englishCV.png" width="200px" /></a></li>
+      <li><a href="#"><img src="frenchCV.png" width="200px" /></a></li>
+    </ul>
   </div>
 </template>
 

0b625d3271804cdd589d5fd95d2b64039b56296c
Samuel Lando <samuel.lando@aol.com>
2020-08-28 11:51:25 -0400

    Layout portfolio

--- src/components/view/Portfolio.vue
+++ src/components/view/Portfolio.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="portfolio">
     <Heading text="Portfolio" />
+    <ul>
+      <a href="https://devpost.com/software/findspot"><li><span>StudySpotter</span></li></a>
+      <a href="https://devpost.com/software/howru"><li><span>HowAreYou</span></li></a>
+      <a href="https://github.com/samuellando/pomodoro"><li><span>Pomodoro</span></li></a>
+      <a href="https://github.com/samuellando"><li><span>Source Code</span></li></a>
+    </ul>
   </div>
 </template>
 

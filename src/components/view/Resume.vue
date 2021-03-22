2f0ae6c1ce4ce89abc7081913158e27cf713fb62
Samuel Lando <samuel.lando@aol.com>
2021-03-22 16:39:03 -0400

    update cv and remove french version

--- src/components/view/Resume.vue
+++ src/components/view/Resume.vue
@@ -3,7 +3,6 @@
     <Heading text="Resume" />
     <ul>
       <li><a href="englishCV.pdf"><img src="englishCV.png" width="200px" /></a></li>
-      <li><a href="frenchCV.pdf"><img src="frenchCV.png" width="200px" /></a></li>
     </ul>
   </div>
 </template>

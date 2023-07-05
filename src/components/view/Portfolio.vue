4d30d044449356448accba4af47ece069adf4997
Samuel Lando <samuel.lando@aol.com>
2023-07-05 13:58:56 -0400

    Update personal website

--- src/components/view/Portfolio.vue
+++ src/components/view/Portfolio.vue
@@ -4,8 +4,11 @@
     <ul>
       <a href="https://devpost.com/software/findspot"><li><span>StudySpotter</span></li></a>
       <a href="https://devpost.com/software/howru"><li><span>HowAreYou</span></li></a>
+      <a href="https://hackline.io"><li><span>HackLine</span></li></a>
+      <a href="https://github.com/samuellando/anyrest"><li><span>AnyRest</span></li></a>
+      <a href="https://github.com/samuellando/hackline"><li><span>HackLine</span></li></a>
       <a href="https://github.com/samuellando/pomodoro"><li><span>Pomodoro</span></li></a>
-      <a href="https://github.com/samuellando"><li><span>Source Code</span></li></a>
+      <a href="https://github.com/samuellando"><li><span>Github</span></li></a>
     </ul>
   </div>
 </template>

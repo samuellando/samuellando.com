372ef7ea86a9a2f2d84edb8ceee1b7d5392ffea7
Samuel Lando <samuel.lando@aol.com>
2025-02-19 12:21:22 -0500

    Add testing for the asset store

---
+++ internal/store/asset/store_test.go
@@ -0,0 +1,211 @@
+package asset
+
+import (
+	"database/sql"
+	"testing"
+
+	"samuellando.com/internal/db"
+	"samuellando.com/internal/testutil"
+)
+
+func setupStore() (Store, *sql.DB) {
+	if err := testutil.ResetDb(); err != nil {
+		panic(err)
+	}
+	con := db.ConnectPostgres(testutil.GetDbCredentials())
+	migrations, err := testutil.GetMigrationsPath()
+	if err != nil {
+		panic(err)
+	}
+	if err := db.ApplyMigrations(con, func(o *db.Options) {
+		o.MigrationsDir = migrations
+		o.Logger = testutil.CreateDiscardLogger()
+	}); err != nil {
+		panic(err)
+	}
+	return CreateStore(con), con
+}
+
+func teardownStore(s Store) {
+	s.db.Close()
+	testutil.ResetDb()
+}
+
+func TestStoreAdd(t *testing.T) {
+	store, _ := setupStore()
+	defer teardownStore(store)
+
+	asset := CreateProto(func(fields *AssetFields) {
+		fields.Name = "Test Asset"
+		fields.Content = []byte("Test Content")
+	})
+
+	err := store.Add(&asset)
+	if err != nil {
+		t.Fatalf("failed to add asset: %v", err)
+	}
+
+	if asset.Id() == 0 {
+		t.Error("expected asset ID to be non-zero after adding")
+	}
+}
+
+func TestStoreGetById(t *testing.T) {
+	store, _ := setupStore()
+	defer teardownStore(store)
+
+	asset := CreateProto(func(fields *AssetFields) {
+		fields.Name = "Test Asset"
+		fields.Content = []byte("Test Content")
+	})
+
+	err := store.Add(&asset)
+	if err != nil {
+		t.Fatalf("failed to add asset: %v", err)
+	}
+
+	retrievedAsset, err := store.GetById(asset.Id())
+	if err != nil {
+		t.Fatalf("failed to get asset by ID: %v", err)
+	}
+
+	if retrievedAsset.Name() != asset.Name() {
+		t.Errorf("expected asset Name to be %s, got %s", asset.Name(), retrievedAsset.Name())
+	}
+}
+
+func TestStoreGetByName(t *testing.T) {
+	store, _ := setupStore()
+	defer teardownStore(store)
+
+	asset := CreateProto(func(fields *AssetFields) {
+		fields.Name = "Test Asset"
+		fields.Content = []byte("Test Content")
+	})
+
+	err := store.Add(&asset)
+	if err != nil {
+		t.Fatalf("failed to add asset: %v", err)
+	}
+
+	retrievedAsset, err := store.GetByName(asset.Name())
+	if err != nil {
+		t.Fatalf("failed to get asset by name: %v", err)
+	}
+
+	if retrievedAsset.Id() != asset.Id() {
+		t.Errorf("expected asset id to be %d, got %d", asset.Id(), retrievedAsset.Id())
+	}
+}
+
+func TestStoreGetAll(t *testing.T) {
+	store, _ := setupStore()
+	defer teardownStore(store)
+
+	asset1 := CreateProto(func(fields *AssetFields) {
+		fields.Name = "Asset 1"
+		fields.Content = []byte("Content 1")
+	})
+	asset2 := CreateProto(func(fields *AssetFields) {
+		fields.Name = "Asset 2"
+		fields.Content = []byte("Content 2")
+	})
+
+	store.Add(&asset1)
+	store.Add(&asset2)
+
+	assets, err := store.GetAll()
+	if err != nil {
+		t.Fatalf("failed to get all assets: %v", err)
+	}
+
+	if len(assets) != 2 {
+		t.Errorf("expected 2 assets, got %d", len(assets))
+	}
+}
+
+func TestStoreFilter(t *testing.T) {
+	store, _ := setupStore()
+	defer teardownStore(store)
+
+	asset1 := CreateProto(func(fields *AssetFields) {
+		fields.Name = "Asset 1"
+		fields.Content = []byte("Content 1")
+	})
+	asset2 := CreateProto(func(fields *AssetFields) {
+		fields.Name = "Asset 2"
+		fields.Content = []byte("Content 2")
+	})
+
+	store.Add(&asset1)
+	store.Add(&asset2)
+
+	filteredStore := store.Filter(func(a *Asset) bool {
+		return a.Name() == "Asset 1"
+	})
+
+	assets, err := filteredStore.GetAll()
+	if err != nil {
+		t.Fatalf("failed to filter assets: %v", err)
+	}
+
+	if len(assets) != 1 || assets[0].Name() != "Asset 1" {
+		t.Errorf("expected 1 asset with name 'Asset 1', got %d with name %s", len(assets), assets[0].Name())
+	}
+}
+
+func TestStoreGroup(t *testing.T) {
+	store, _ := setupStore()
+	defer teardownStore(store)
+
+	asset1 := CreateProto(func(fields *AssetFields) {
+		fields.Name = "Asset 1"
+		fields.Content = []byte("Content 1")
+	})
+	asset2 := CreateProto(func(fields *AssetFields) {
+		fields.Name = "Asset 2"
+		fields.Content = []byte("Content 2")
+	})
+
+	store.Add(&asset1)
+	store.Add(&asset2)
+
+	grouped := store.Group(func(a *Asset) string {
+		return a.Name()
+	})
+
+	if grouped.Len() != 2 {
+		t.Errorf("expected 2 groups, got %d", grouped.Len())
+	}
+}
+
+func TestStoreSort(t *testing.T) {
+	store, _ := setupStore()
+	defer teardownStore(store)
+
+	asset1 := CreateProto(func(fields *AssetFields) {
+		fields.Name = "Asset B"
+		fields.Content = []byte("Content B")
+	})
+	asset2 := CreateProto(func(fields *AssetFields) {
+		fields.Name = "Asset A"
+		fields.Content = []byte("Content A")
+	})
+
+	store.Add(&asset1)
+	store.Add(&asset2)
+
+	sortedStore := store.Sort(func(a1, a2 *Asset) bool {
+		return a1.Name() < a2.Name()
+	})
+
+	assets, err := sortedStore.GetAll()
+	if err != nil {
+		t.Fatalf("failed to sort assets: %v", err)
+	}
+
+	if assets[0].Name() != "Asset A" || assets[1].Name() != "Asset B" {
+		t.Errorf("expected assets sorted by name, got %s and %s", assets[0].Name(), assets[1].Name())
+	}
+}
+

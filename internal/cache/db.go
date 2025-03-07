d2456eb4d8fe01652d02e9f27c655d31a2c85757
Samuel Lando <samuel.lando@aol.com>
2025-03-07 15:52:20 -0500

    Migrate cache module to sqlc

--- internal/cache/db.go
+++ internal/cache/db.go
@@ -1,9 +1,12 @@
 package cache
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"time"
+
+	"samuellando.com/data"
 )
 
 // Attempt to retrieve a cache entry from the external database.
@@ -11,21 +14,16 @@ import (
 func dbCacheGet(key string, options CacheOptions) (cacheElement, error) {
 	if options.Db != nil {
 		log.Println("Checking external cache")
-		var value []byte
-		var validto time.Time
-		row := options.Db.QueryRow(`
-            SELECT 
-                cache_value, validto 
-            FROM cache 
-            WHERE cache_key = $1
-        `, key)
-		if err := row.Scan(&value, &validto); err != nil {
+		ctx := context.TODO()
+		queries := data.New(options.Db)
+		row, err := queries.GetCacheByKey(ctx, key)
+		if err != nil {
 			log.Println("Cache from db error: ", err)
 			return cacheElement{}, err
 		}
-		if time.Since(validto) <= 0 {
-			log.Println("External cache hit", time.Since(validto))
-			return cacheElement{value: value, validTo: validto}, nil
+		if time.Since(row.ValidTo) <= 0 {
+			log.Println("External cache hit", time.Since(row.ValidTo))
+			return cacheElement{value: row.CacheValue, validTo: row.ValidTo}, nil
 		}
 		return cacheElement{}, fmt.Errorf("db cache miss")
 	} else {
@@ -36,23 +34,13 @@ func dbCacheGet(key string, options CacheOptions) (cacheElement, error) {
 // Insert or update a cache entry in the external database.
 func dbCacheUpdate(key string, elem cacheElement, options CacheOptions) {
 	if options.Db != nil {
-		tx, err := options.Db.Begin()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		defer tx.Rollback()
-		_, err = tx.Exec(`
-			INSERT INTO cache (cache_key, cache_value, validto) VALUES
-                ($1, $2, $3)
-			ON CONFLICT (cache_key) DO UPDATE SET
-                cache_value = EXCLUDED.cache_value,
-                validto = EXCLUDED.validto
-        `, key, elem.value, elem.validTo)
-		if err != nil {
-			log.Println(err)
-		}
-		err = tx.Commit()
+		ctx := context.TODO()
+		queries := data.New(options.Db)
+		err := queries.SetCacheByKey(ctx, data.SetCacheByKeyParams{
+			CacheKey:   key,
+			CacheValue: elem.value,
+			ValidTo:    elem.validTo,
+		})
 		if err != nil {
 			log.Println(err)
 		}
